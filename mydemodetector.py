e01093bf046473f40b57fdb9b10d7f4e9184a9d9
Kaikai Liu <kaikai.liu@sjsu.edu>
2020-10-26 12:09:39 -0700

    Change test data folder

--- mydemodetector.py
+++ mydemodetector.py
@@ -9,16 +9,16 @@ from utils import detectimage
 
 class detectorargs:
     modelname = 'fasterrcnn_resnet50_fpn'#not used here
-    modelbasefolder = './models/ModelZoo/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8/saved_model/'
+    modelbasefolder = '../models/ModelZoo/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8/saved_model/'
     modelfilename='faster_rcnn_resnet50_v1_640x640_coco17_tpu-8' #not used
     #showfig='True'
-    labelmappath = './models/research/object_detection/data/mscoco_label_map.pbtxt'
+    labelmappath = '../models/research/object_detection/data/mscoco_label_map.pbtxt'
     threshold = 0.3
 
 if __name__ == "__main__":
     mydetector = TF2Detector.MyTF2Detector(detectorargs)
 
-    imgpath=os.path.join('.', "test.jpg")
+    imgpath=os.path.join('./testdata', "traffic1.jpg")
     print(imgpath)
     bbox_xyxy, pred_labels, cls_conf=detectimage.detectoneimage_novis(imgpath, mydetector)
     print(pred_labels)
