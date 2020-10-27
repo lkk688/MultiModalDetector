16fda6469df0aee742697d7e70d0b0b3a512a95b
Kaikai Liu <kaikai.liu@sjsu.edu>
2020-10-26 18:04:10 -0700

    add detect image folder

--- utils/detectimage.py
+++ utils/detectimage.py
@@ -3,10 +3,14 @@ import matplotlib
 import os
 import time
 import cv2
+import glob
+from pathlib import Path
+
 
 import importlib
 from utils import plotresults
-importlib.reload(plotresults)
+importlib.reload(plotresults)#only used for debug
+
 
 def detectoneimage(imgpath, mydetector):
     start = time.time()
@@ -46,3 +50,41 @@ def detectoneimage_novis(imgpath, mydetector):
 
     pred_labels = [mydetector.FULL_LABEL_CLASSES[i] for i in list(cls_ids) ]
     return bbox_xyxy, pred_labels, cls_conf
+
+def detectoneimagefolder_tovideo(imgpath, mydetector, outputvideopath):
+    imagepath=glob.glob(imgpath)
+    imglen=len(imagepath)
+    print("Total image:", imglen)
+    imgidx=0
+    results = []
+        
+    first_im = cv2.imread(imagepath[0])
+    imageshape=first_im.shape
+    im_width=imageshape[1]#2720#800
+    im_height=imageshape[0]#1530#600
+    print("Image width: ", im_width)
+    print("Image height: ", im_height)
+    fourcc = cv2.VideoWriter_fourcc('M','P','4','V')#cv2.VideoWriter_fourcc(*'MJPG')#cv2.VideoWriter_fourcc('M','P','4','V') #cv2.VideoWriter_fourcc(*'MJPG')
+    videooutput = cv2.VideoWriter(self.save_video_path, fourcc, 20, (im_width, im_height))
+        
+    for imgidx in range(imglen): #imglen  while self.vdo.grab():
+        filepath=imagepath[imgidx]
+        f_image_path=Path(filepath)#convert str to Path object
+        f_image_name = f_image_path.name
+        print(f_image_name)#image file name
+        imgfiles=os.path.splitext(f_image_name)
+        imageid=imgfiles[0] #image filename without .jpg
+
+        start = time.time()
+        #_, im = self.vdo.retrieve()
+        ori_im = cv2.imread(filepath)
+        print(ori_im.shape)
+        im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
+        #Perform detection
+        bbox_xyxy, cls_ids, cls_conf = mydetector.detect(im)
+        end = time.time()
+        print("Detection time: {:.03f}s, fps: {:.03f}, detection numbers: {}".format(end - start, 1 / (end - start), len(bbox_xyxy)))
+
+        plotresults.show_imagewithscore_bbxyxy(im, bbox_xyxy, cls_ids, cls_conf, imgpath, mydetector.FULL_LABEL_CLASSES, outputvideopath+imageid+''.jpg')
+    
+    print("Finished all detections")
